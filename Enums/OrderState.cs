340eafcd51c3b694ba3662068c582d5169d73e5b
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-02-22 13:14:33 +0100

    Create dotnet.yml (#2)
    * Create dotnet.yml
    
    * Fix project name
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- Enums/OrderState.cs
+++ Enums/OrderState.cs
@@ -1,11 +1,17 @@
 ﻿namespace KinoDev.Shared.Enums
 {
+    /// <summary>
+    /// IMPORTANT!
+    /// This values are used in the database.
+    /// Do not chang numbers or order of the values.
+    /// Add new values at the end of the list.
+    /// </summary>
     public enum OrderState
     {
-        New,
-        Pending,
-        Processing,
-        Completed,
-        Cancelled
+        New = 0,
+        Pending = 10,
+        Processing = 20,
+        Completed = 30,
+        Cancelled = 40
     }
 }
