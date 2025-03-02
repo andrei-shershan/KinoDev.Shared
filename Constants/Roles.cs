876a096d8531ec4b63c2bbbda4f05063a16efdfe
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-03-02 10:52:17 +0100

    Add Roles (#5)
    * Add Roles
    
    * Fix Version

---
+++ Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace KinoDev.Shared.Constants
+{
+    public class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string Cashier = "Cashier";
+        public const string Customer = "Customer";
+    }
+}
