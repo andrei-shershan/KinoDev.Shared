28fd665ce0f86980c1b1669476f4a00e5b6531ab
Andrei Shershan <mail.shershan@gmail.com>
2025-03-07 19:41:03 +0100

    Movies DTO

---
+++ DtoModels/MovieDto.cs
@@ -0,0 +1,17 @@
+﻿namespace KinoDev.Shared.DtoModels
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+
+        public int Duration { get; set; }
+
+        public string Url { get; set; }
+    }
+}
