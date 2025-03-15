2df9e4ef565b1e1f6943f3878344f9fc2290a038
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-03-15 19:56:12 +0100

    Order models (#14)

---
+++ DtoModels/Tickets/TicketDto.cs
@@ -0,0 +1,11 @@
+namespace KinoDev.Shared.DtoModels.Tickets
+{
+    public class TicketDto
+    {
+        public int ShowTimeId { get; set; }
+
+        public int SeatId { get; set; }
+
+        public int OrderId { get; set; }
+    }
+}
