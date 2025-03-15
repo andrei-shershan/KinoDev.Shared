9fd28ae1414cdb910d4e364ae9cd6e6323d3da1e
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-03-15 20:06:17 +0100

    Fix Ticket DTO with ID (#15)

--- DtoModels/Tickets/TicketDto.cs
+++ DtoModels/Tickets/TicketDto.cs
@@ -2,6 +2,8 @@ namespace KinoDev.Shared.DtoModels.Tickets
 {
     public class TicketDto
     {
+        public int Id { get; set; }
+        
         public int ShowTimeId { get; set; }
 
         public int SeatId { get; set; }
