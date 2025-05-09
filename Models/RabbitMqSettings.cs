69993960931132767e4506f6c4c8c46bbd36d6cd
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-05-09 22:13:51 +0200

    message broker (#24)
    * message broker
    
    * fix version

---
+++ Models/RabbitMqSettings.cs
@@ -0,0 +1,10 @@
+namespace KinoDev.Shared.Models
+{
+    public class RabbitMqSettings
+    {
+        public string HostName { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
