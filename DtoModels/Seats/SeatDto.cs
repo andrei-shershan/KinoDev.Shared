c548b872c59c19a3da4069a8ca99caac76cd8bd7
Andrei Shershan <mail.shershan@gmail.com>
2025-05-26 22:01:12 +0200

    Add Hall Summary with Seats

---
+++ DtoModels/Seats/SeatDto.cs
@@ -0,0 +1,13 @@
+namespace KinoDev.Shared.DtoModels.Seats
+{
+    public class SeatDto
+    {
+        public int Id { get; set; }
+
+        public int HallId { get; set; }
+
+        public int Row { get; set; }
+
+        public int Number { get; set; }
+    }
+}
