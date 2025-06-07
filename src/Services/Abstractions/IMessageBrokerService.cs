ff989e45af204dfeff29a18c4462d2d300115548
Andrei Shershan <mail.shershan@gmail.com>
2025-06-07 21:03:41 +0200

    Cleanup

--- src/Services/Abstractions/IMessageBrokerService.cs
+++ src/Services/Abstractions/IMessageBrokerService.cs
@@ -2,15 +2,8 @@ namespace KinoDev.Shared.Services.Abstractions
 {
     public interface IMessageBrokerService
     {
-        Task PublishAsync(object data, string subscription, string key = "");
-
-        Task SubscribeAsync(string subscription, string queueName, Func<string, Task> callback, string key = "");
-
         Task SendMessageAsync(string queueName, object data);
 
-        Task SubscribeAsync(string queueName, Func<string, Task> callback);
-
-        Task SubscribeAsync<T>(string queueName, Func<T, Task> callback) 
-        where T : class;
+        Task SubscribeAsync<T>(string queueName, Func<T, Task> callback) where T : class;
     }
 }
