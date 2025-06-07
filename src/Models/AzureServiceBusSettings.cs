0a1acb1de2349be3b71b58b32dbc02af799ea8c4
Andrei Shershan <mail.shershan@gmail.com>
2025-06-07 10:58:48 +0200

    Add Azure Service Bus

---
+++ src/Models/AzureServiceBusSettings.cs
@@ -0,0 +1,7 @@
+namespace KinoDev.Shared.Models
+{
+    public class AzureServiceBusSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
--- src/Services/Abstractions/IMessageBrokerService.cs
+++ src/Services/Abstractions/IMessageBrokerService.cs
@@ -1,9 +1,13 @@
-namespace KinoDev.Shared.Services
+namespace KinoDev.Shared.Services.Abstractions
 {
     public interface IMessageBrokerService
     {
         Task PublishAsync(object data, string subscription, string key = "");
 
         Task SubscribeAsync(string subscription, string queueName, Func<string, Task> callback, string key = "");
+
+        Task SendMessageAsync(string queueName, string message);
+
+        Task<string> ReceiveMessageAsync(string queueName, CancellationToken cancellationToken = default);
     } 
 }
